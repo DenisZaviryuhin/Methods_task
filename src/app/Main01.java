8ee711ace37d92ec7259d74436bee729d848b16e
DenisZaviryuhin <denzava30@gmail.com>
2023-09-21 23:57:19 +0300

    Update app info

--- src/app/Main01.java
+++ src/app/Main01.java
@@ -1,60 +1,32 @@
 package app;
 
+
 import java.util.Scanner;
 import java.util.Locale;
 
 public class Main01 {
-    static final String CURRENCY = "USD";
-    static double amount;
-    static double discount;
-    static double toPay;
+
 
     public static void main(String[] args) {
-
-        enterSum();
-        calcDiscount();
-        calcPay();
-        showData();
-    }
-
-    private static void enterSum() {
-        //Вводимо загальну сумму закупівель
-        //Отримуємо розрахунок
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.ENGLISH);
         System.out.println("Enter purchase amount : ");
-        amount = scanner.nextDouble();
+        double amount = scanner.nextDouble();
+        scanner.close();
+
+        CalcService service = new CalcService();
+        Outputer output = new Outputer();
+
+        double discount = service.calcDiscount(amount);
+        double purchase = service.calcTotal(amount, discount);
+        output.getOutput(discount);
+        output.getOutputTotal(purchase);
 
 
-    }
-
-    private static void showData() {
-
-        System.out.printf("Total amount , %s : %.2f \nDiscount amount , %s : %.2f \nTotal to pay , %s : %.2f",
-                CURRENCY, amount, CURRENCY, discount, CURRENCY, toPay);
 
 
-    }
-
-    private static void calcDiscount() {
-        int limit1 = 5000;
-        int limit2 = 10000;
-        int limit3 = 10001;
-
-        if (amount <= limit1) {
-            discount = (amount / 100) * 5;
-
-        } else if (amount > limit1 & amount <= limit2) {
-            discount = (amount / 100) * 10;
-
-        } else if (amount >= limit3) {
-            discount = (amount / 100) * 15;
-        }
 
 
-    }
-    private static void calcPay() {
-        toPay = amount - discount;
 
     }
 }
